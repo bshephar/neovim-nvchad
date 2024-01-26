2c0efb715788c571ffd1669f0539f7fa436320b7
Brendan Shephard <bshephar@redhat.com>
2024-01-26 19:57:10 +1000

    nvchad config initial commit

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,17 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "go",
+    "python",
+    "rust",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
